ba105b371804875efe283802d765293ce471ab42
harman <harman.sarwara@gmail.com>
2020-03-08 18:35:18 -0400

    first commit

---
+++ models/Events.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+let Events = new Schema({
+  eventType: String,
+  time: {type: Date, default: Date.now},
+  userName: String,
+  socketID: String,
+  room: String
+})
+
+module.exports = mongoose.model('Events', Events)
---
+++ models/Room.js
@@ -0,0 +1,11 @@
+/* eslint-disable one-var */
+// eslint-disable-next-line one-var
+// eslint-disable-next-line no-unused-vars
+var mongoose = require('mongoose'), Schema = mongoose.Schema
+
+var RoomSchema = new mongoose.Schema({
+  room_name: String,
+  created_date: { type: Date, default: Date.now }
+})
+
+module.exports = mongoose.model('Room', RoomSchema)
