a8c72834a778e3b22e441b0ff2de45e60c87a178
harman <harman.sarwara@gmail.com>
2020-04-07 18:31:10 -0400

    changes made to add

--- models/Room.js
+++ models/Room.js
@@ -9,7 +9,27 @@ var RoomSchema = new mongoose.Schema({
     type: Schema.Types.ObjectId,
     ref: 'Chat'
   }],
-  created_date: { type: Date, default: Date.now }
-})
+  status: String,
+  // edit_date: { type: Date, default: Date.now },
+  // created_date: { type: Date, default: Date.now }
+},
+{
+  id: false,
+  toObject: {
+      virtuals: true,
+      getters: true
+  },
+  toJSON: {
+      virtuals: true,
+      getters: true,
+      setters: false
+  },
+  timestamps: true
+});
+
+RoomSchema.pre('find', function () {
+  this.where({ is_active: { $ne: false } });
+});
+
 
 module.exports = mongoose.model('Room', RoomSchema)
